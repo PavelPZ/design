8420302082ef40c033602b0189da82e06ec6ffe5
PZ <pzika@langmaster.cz>
2016-11-16 10:11:28 +0100

    v

---
+++ rt/node-cmd.ts
@@ -0,0 +1,13 @@
+﻿import * as fs from "fs";
+
+const basicPath = '../../design-rt/';
+
+let classMap = JSON.parse(fs.readFileSync(basicPath + 'class-map.json', "utf8")); 
+
+for (var p in classMap) {
+  if (p == '') continue;
+  let fn = 'react-toolbox/' + p + '.js';
+  console.log(fn);
+  console.log(JSON.stringify(classMap[p],null,2));
+  fs.writeFileSync(fn, JSON.stringify(classMap[p]));
+}
